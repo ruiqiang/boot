130927ac6a0a3ce54b37c1312626b981cdff5b3e
rxqjack@126.com <rxqjack@126.com>
2019-07-12 15:25:30 +0800

    springboot learning

--- src/main/java/com/ruiqiang/sp/SpApplication.java
+++ src/main/java/com/ruiqiang/sp/SpApplication.java
@@ -3,6 +3,7 @@ package com.ruiqiang.sp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
